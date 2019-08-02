c53c13c5081eec02d2e121876d9f329cb6e02cdd
oscaredu22 <oscaredu03@hotmail.com>
2019-08-02 14:30:48 -0500

    listo

--- servidorProyectoIntegrador/server.js
+++ servidorProyectoIntegrador/server.js
@@ -1,12 +1,16 @@
 var express = require('express');
+let bcrypt = require('bcrypt');
 var bodyParser = require('body-parser');
 var port = 3000;
 var knex = require('./db/knex');
+var jwt = require('jsonwebtoken');
 
 var app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
+
+var JWT_SECRET = '123456';
 
 //CORS
 app.use((req, res, next) => {
@@ -61,6 +65,82 @@ app.get('/getPasteles', function(req, res) {
     })    
 })
 
+//POST LOGEO
+app.post('/login', function(req, res) {
+    console.log(req.body)
+    let usuario = req.body.usuario
+    let password = req.body.password
+    console.log(usuario)
+    knex('usuarioLogin').where('usuario', usuario)
+    .then(resultado => {
+        console.log(resultado)
+        if(resultado.length > 0){
+            bcrypt.compare(password, resultado[0].password).then(ok => {
+                if(ok){
+                    resultado[0].password = '********ll';
+                    console.log(resultado[0])
+                    let tokenC = jwt.sign(resultado[0], 'hdfkasjhfjasdhlajhsldfjha');
+                    
+                    console.log(tokenC);
+                    return res.status(200).json({
+                        ok: true,
+                        datos: resultado,
+                        token: tokenC,
+                        mensaje: `Existen ${resultado.length} registros en la consulta`,
+                        mensaje2: `Haz iniciado sesion`
+                    })
+                }
+                else{
+                    return res.status(200).json({
+                        ok: false,
+                        datos: null,
+                        mensaje: `Usuario o contraseña incorrecta`
+                    })
+                }
+            })
+        }
+    })
+    .catch((error) => {
+        return res.status(500).json({
+            ok: false,
+            datos: null,
+            mensaje: `Error del servidor: ${error}`
+        })
+    }) 
+})
+
+
+
+//INSERT USUARIO REGISTRO
+app.post('/agregarUsuario', function(req, res) {
+    let usuario = req.body.usuario;
+    let password = '';
+    bcrypt.hash(req.body.password, 10, function(err, hash){
+        password = hash;
+        console.log(password)
+        return knex('usuarioLogin').insert({
+            usuario : usuario,
+            password : password
+        })
+        .then(resultado => {
+            return res.status(200).json({
+                ok: true,
+                datos: resultado,
+                mensaje: `Existen ${resultado.length} registros en la consulta`
+            })
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                ok: false,
+                datos: null,
+                mensaje: `Error del servidor: ${error}`
+            })
+        })    
+    })
+    
+})
+
+
 //INSERT PRODUCTO
 app.post('/agregarProducto', function(req, res) {
     // let tabla = req.body.tabla
@@ -88,29 +168,6 @@ app.post('/agregarProducto', function(req, res) {
     })    
 })
 
-//INSERT USUARIO
-app.post('/agregarUsuario', function(req, res) {
-    // let tabla = req.body.tabla
-    // let datos = JSON.stringify(req.body.datos)
-    return knex('usuarioLogin').insert({
-        "usuario" : req.body.usuario,
-        "password" : req.body.password
-    })
-    .then(resultado => {
-        return res.status(200).json({
-            ok: true,
-            datos: resultado,
-            mensaje: `Existen ${resultado.length} registros en la consulta`
-        })
-    })
-    .catch((error) => {
-        return res.status(500).json({
-            ok: false,
-            datos: null,
-            mensaje: `Error del servidor: ${error}`
-        })
-    })    
-})
 
 //INSERT PEDIDO
 app.post('/agregarPedido', function(req, res) {
@@ -138,15 +195,67 @@ app.post('/agregarPedido', function(req, res) {
     })    
 })
 
-//INSERT ADMINISTRADOR
+//POST LOGEO ADMIN
+app.post('/logAdmin', function(req, res) {
+    console.log(req.body)
+    let nombre = req.body.nombre
+    let apellido = req.body.apellido
+    let password = req.body.password
+    console.log(nombre)
+    console.log(apellido)
+    knex('administracion').where({'nombre': nombre, 'apellido': apellido})
+    .then(resultado => {
+        console.log(resultado)
+        if(resultado.length > 0){
+            bcrypt.compare(password, resultado[0].password).then(ok => {
+                if(ok){
+                    resultado[0].password = '********ll';
+                    console.log(resultado[0])
+                    let tokenC = jwt.sign(resultado[0], 'asdaslkdjalksjdlsakldj');
+                    
+                    console.log(tokenC);
+                    return res.status(200).json({
+                        ok: true,
+                        datos: resultado,
+                        token: tokenC,
+                        mensaje: `Existen ${resultado.length} registros en la consulta`,
+                        mensaje2: `Haz iniciado sesion`
+                    })
+                }
+                else{
+                    return res.status(200).json({
+                        ok: false,
+                        datos: null,
+                        mensaje: `Alguno de los datos estan incorrectos`
+                    })
+                }
+            })
+        }
+    })
+    .catch((error) => {
+        return res.status(500).json({
+            ok: false,
+            datos: null,
+            mensaje: `Error del servidor: ${error}`
+        })
+    }) 
+})
+
+
+//INSERT ADMINISTRADOR REGISTRO
 app.post('/agregarAdmin', function(req, res) {
-    // let tabla = req.body.tabla
-    // let datos = JSON.stringify(req.body.datos)
+    let nombre = req.body.nombre;
+    let apellido = req.body.apellido;
+    let cargo = req.body.cargo;
+    let password = '';
+    bcrypt.hash(req.body.password, 10, function(err, hash){
+        password = hash;
+        console.log(password)
     return knex('administracion').insert({
-        "nombre" : req.body.nombre,
-        "apellido" : req.body.apellido,
-        "cargo": req.body.cargo,
-        "password": req.body.password
+        nombre : nombre,
+        apellido : apellido,
+        cargo: cargo,
+        password: password
     })
     .then(resultado => {
         return res.status(200).json({
@@ -161,30 +270,10 @@ app.post('/agregarAdmin', function(req, res) {
             datos: null,
             mensaje: `Error del servidor: ${error}`
         })
+    })
     })    
 })
 
-//UPDATE
-app.put('/actualizar', function(req, res) {
-    let tabla = req.body.tabla
-    let campo = req.body.campo
-    let dato = req.body.dato
-    knex(tabla)/*.where("id", req.params.id)*/.update(campo, dato)
-    .then(resultado => {
-        return res.status(200).json({
-            ok: true,
-            datos: resultado,
-            mensaje: `Existen ${resultado.length} registros en la consulta`
-        })
-    })
-    .catch((error) => {
-        return res.status(500).json({
-            ok: false,
-            datos: null,
-            mensaje: `Error del servidor: ${error}`
-        })
-    })    
-})
 
 //DELETE PEDIDOS
 app.delete('/eliminar/:id', function(req, res) {
@@ -210,3 +299,23 @@ app.delete('/eliminar/:id', function(req, res) {
 app.listen(port, function() {
     console.log("El servidor se ejecuta en el puerto: ", port);
 })
+
+// if(req.body){
+//     var user = req.body;
+//     console.log(user);
+
+//     if(usuario === req.body.usuario && password === req.body.password){
+//         var token = jwt.sign(user, JWT_SECRET);
+//         res.status(200).send({
+//             sigin: user,
+//             token: token
+//         });
+//     }
+//     else{
+//         res.status(403).send({
+//             errorMensaje: 'error'
+//         })
+//     }
+// }    
+
+// knex.select("usuario","password").from('usuarioLogin')
